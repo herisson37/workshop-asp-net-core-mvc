6c00042674d8035c40502bebb61f08deeda371c4
Herrisson Vinicius <herrissonvinicius@gmail.com>
2020-03-24 18:03:35 -0400

    SellerFormViewModel and  Department select component

---
+++ SalesWebMvc/Models/ViewModels/SellerFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SalesWebMvc.Models.ViewModels
+{
+    public class SellerFormViewModel
+    {
+        public Seller Seller { get; set; }
+        public IEnumerable<Department> Departments { get; set; }
+    }
+}
