704a22c92dfea33eb6393f2cc00586b695c5d252
Herrisson Vinicius <herrissonvinicius@gmail.com>
2020-03-28 13:42:04 -0400

    Seller details and eager loading

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -68,5 +68,22 @@ namespace SalesWebMvc.Controllers
 
         }
 
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
     }
 }
